9bf7dd600fd83c77b354aa40e9536cb2bef6af4c
easa <eisanodehi@gmail.com>
2019-10-01 12:13:16 +0330

    add 2 new features
    longest word
    tiniest word
    
    as base common module parseWords is added to parsing words

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ var app = require('./app/index')
 
 var str = app.exStr(`this is 'hello' and this is "world" !`)
 console.log(str)
+
+var long = app.longestWord('this is a veryLong9 word')
+var long1 = app.tinestWord('this is a veryshort word')
+console.log(`the longest string is ${long} long and tiniest is ${long1}`)
