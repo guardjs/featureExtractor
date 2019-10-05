88cee9b757dbe248bc1aaf86d5b6245e8f0fa0a3
easa <eisanodehi@gmail.com>
2019-10-06 01:16:18 +0330

    update app js accept cmd-line args update utf feature (return len

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 
 
-/** this is a test page by now */
+/**
+ *  Developed by easa 
+ *  this is a test page by now :
+ * 
+ * requisites:            nodejs
+ * $ node app "string"          // will extract the argument or the sample string
+ * string is optional
+*/
+
 var app = require('./app/index')
 
-var result = [], str = `this is 'hello' and this is var word="world" !`
+var result = [], str = process.argv[2] || `this is 'hello' and this is var word="world" !`
+
 
 // call all functions
 Object.keys(app).forEach(func => { result.push(app[func](str)) })
