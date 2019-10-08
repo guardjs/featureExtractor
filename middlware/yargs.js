5588a3d83745568e10e658680b1b9888947973ac
easa <eisanodehi@gmail.com>
2019-10-08 13:43:09 +0330

    yrags is working ok

--- middlware/yargs.js
+++ middlware/yargs.js
@@ -14,37 +14,20 @@ var opt = {}
 yargs.command('serve [d]', 'creates the features', (yargs) => {
   yargs.positional('d', {
     describe: 'directory to bind features from the javascript files insied it',
-    default: './samples'
-  })
-  yargs.positional('r', {
-    describe: 'result file, or simply use the cmd defualt command to save result on a file ">> filename"',
-    default: './result.csv'
+    default: './sample'
   })
 }, (argv) => {
-  if (argv) console.error(`GUARDJS: process is started checkout ${argv.n}`)
-  opt.resultFile = argv.r
+  opt.isRawRequired = true
   opt.folder = argv.d
-  opt.isRawRequired = argv.v
-  opt.isFeatureNameRequired = argv.n
 }).option('directory', {
   alias: 'd',
   demandOption: true,
-  default: './samples'
+  default: './sample'
 }).option('result', {
   alias: 'r',
-  default: './result.csv'
-}).option('name', {
-  type: Boolean,
-  alias: 'n',
-  default: false
-}).option('values', {
-  type: Boolean,
-  alias: 'v',
-  default: false
+  default: './result/default.csv'
 }).command('feature', 'show the features list', (yargs) => { }, (param) => {
   opt.isFeatureNameRequired = true
 }).argv
-
-console.log(opt)
 
 module.exports = opt
