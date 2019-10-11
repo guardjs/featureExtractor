42a9bb880807afa2a0a926cb1863ab6ac7436800
easa <eisanodehi@gmail.com>
2019-10-11 14:12:01 +0330

    Fix dependency injection

--- app.js
+++ app.js
@@ -10,24 +10,9 @@
  * to save result on file use "> filename" command
  */
 var mid = require('./middlware/')
-var app = require('./app/')
 
-var result = [],
-    opt = mid.cmd || {},
-    str = opt.input || `this is a 'hello' and this iframe is var word="iframe" !`
+var opt = mid.cmd || {}
+opt.input = opt.input || `this is a 'hello' and this iframe is var word="iframe" !`
 
-// result the list of feature names
-if (opt.isFeatureNameRequired)
-    console.log(Object.keys(app).reduce((i, c) => i += ', ' + c))
-
-if (opt.isFeatureCountRequired)
-    console.log(Object.keys(app).length)
-
-// break if no need to the features
-if (!opt.isRawRequired) return void 0
-
-// running feature extraction on input string
-Object.keys(app).forEach(func => { result.push(app[func](str)) })
-
-// export result
-console.log(result.reduce((i, c) => i += ', ' + c))
+var app = require('./app/')(opt)
+console.log(app)
